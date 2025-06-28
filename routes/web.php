2b712da7c7e7e0cd405aa3f765702928b3939311
Awais-Koder <awaistayyab27@gmail.com>
2025-06-28 13:23:07 +0500

    Auto commit

--- routes/web.php
+++ routes/web.php
@@ -15,15 +15,15 @@ Route::view('dashboard', 'dashboard')
     ->middleware(['auth', 'verified'])
     ->name('dashboard');
 
-Route::middleware(['auth' , 'personal-verify'])->group(function () {
+Route::middleware(['auth', 'personal-verify'])->group(function () {
     Route::redirect('settings', 'settings/profile');
 
     Route::get('settings/profile', Profile::class)->name('settings.profile');
     Route::get('settings/password', Password::class)->name('settings.password');
     Route::get('settings/appearance', Appearance::class)->name('settings.appearance');
-    Route::get('download/pdf/{id}' , [PdfDownloadController::class , 'downloadPdf'])->name('download.pdf');
-    Route::get('download/submittel/files/{id}' , [SubmittelFilesController::class , 'downloadSubmittelFiles'])->name('download.submittel.files');
+    Route::get('download/pdf/{id}', [PdfDownloadController::class, 'downloadPdf'])->name('download.pdf');
+    Route::get('download/submittel/files/{id}', [SubmittelFilesController::class, 'downloadSubmittelFiles'])->name('download.submittel.files');
 });
 
 
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
